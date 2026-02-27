1fba26b84f4a34c45364d42a7e53731ba5f5f8ce
24ro <Mussasarodrigue@gmail.com>
2026-02-27 16:43:56 -0500

    initial project setup

---
+++ src/main/java/com/rodrigue/mobilemoney/MobilemoneyApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigue.mobilemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilemoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilemoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigue/mobilemoney/MobilemoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigue.mobilemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilemoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
